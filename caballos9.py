73cf514fecf542fb084321da700b6180c7016a6c
Jorgediamanto <99408709+Jorgediamanto@users.noreply.github.com>
2022-09-09 21:53:19 +0200

    Add files via upload

---
+++ caballos9.py
@@ -0,0 +1,165 @@
+# 32 movimientos
+def my_function(x1,efef,m1,m2):
+    ddd = []
+    ramon = 0
+                    
+    ddd.append(efef[m1])
+    ddd.append(efef[m2])
+
+    for z1 in range(20):
+        if ddd==x1[z1]:
+            ramon=1
+    return ramon
+  
+total=0
+efef = []
+
+x1 = [[1,6],[1,8],[2,7],[2,9],[3,4],[3,8],[4,3],[4,9],[4,0],[6,1],[6,7],[6,0],[7,2],[7,6],[8,1],[8,3],[9,4],[9,2],[0,4],[0,6]]
+
+
+for x in range(10):
+    for y in range(10):
+        for x3 in range(10):
+            for x4 in range(10):
+                for x5 in range(10):
+                    for x6 in range(10):
+                        for x7 in range(10):
+                            for x8 in range(10):
+                                for x9 in range(10):
+                                    for x10 in range(10):
+                                        for x11 in range(10):
+                                            for x12 in range(10):
+                                                for x13 in range(10):
+                                                    for x14 in range(10):
+                                                        for x15 in range(10):
+                                                            for x16 in range(10):
+                                                                for x17 in range(10):
+                                                                    for x18 in range(10):
+                                                                        for x19 in range(10):
+                                                                            for x20 in range(10):
+                                                                                for x21 in range(10):
+                                                                                    for x22 in range(10):
+                                                                                        for x23 in range(10):
+                                                                                            for x24 in range(10):
+                                                                                                for x25 in range(10):
+                                                                                                    for x26 in range(10):
+                                                                                                        for x27 in range(10):
+                                                                                                            for x28 in range(10):
+                                                                                                                for x29 in range(10):
+                                                                                                                    for x30 in range(10):
+                                                                                                                        for x31 in range(10):
+                                                                                                                            for x32 in range(10):
+                                                                                                                                for x33 in range(10):
+                                                                                                                                    efef.append(x)
+                                                                                                                                    efef.append(y)
+                                                                                                                                    efef.append(x3)
+                                                                                                                                    efef.append(x4)
+                                                                                                                                    efef.append(x5)
+                                                                                                                                    efef.append(x6)
+                                                                                                                                    efef.append(x7)
+                                                                                                                                    efef.append(x8)
+                                                                                                                                    efef.append(x9)
+                                                                                                                                    efef.append(x10)
+                                                                                                                                    efef.append(x11)
+                                                                                                                                    efef.append(x12)
+                                                                                                                                    efef.append(x13)
+                                                                                                                                    efef.append(x14)
+                                                                                                                                    efef.append(x15)
+                                                                                                                                    efef.append(x16)
+                                                                                                                                    efef.append(x17)
+                                                                                                                                    efef.append(x18)
+                                                                                                                                    efef.append(x19)
+                                                                                                                                    efef.append(x20)
+                                                                                                                                    efef.append(x21)
+                                                                                                                                    efef.append(x22)
+                                                                                                                                    efef.append(x23)
+                                                                                                                                    efef.append(x24)
+                                                                                                                                    efef.append(x25)
+                                                                                                                                    efef.append(x26)
+                                                                                                                                    efef.append(x27)
+                                                                                                                                    efef.append(x28)
+                                                                                                                                    efef.append(x29)
+                                                                                                                                    efef.append(x30)
+                                                                                                                                    efef.append(x31)
+                                                                                                                                    efef.append(x32)
+                                                                                                                                    efef.append(x33)
+                                                                                                                                    #print(verga)
+                                                                                                                                    x2=0
+                                                                                                                                    x22=0
+                                                                                                                                    x222=0
+                                                                                                                                    x2222=0
+                                                                                                                                    x22222=0
+                                                                                                                                    x21=0
+                                                                                                                                    x22=0
+                                                                                                                                    x23=0
+                                                                                                                                    x24=0
+                                                                                                                                    x25=0
+                                                                                                                                    x26=0
+                                                                                                                                    x27=0
+                                                                                                                                    x28=0
+                                                                                                                                    x29=0
+                                                                                                                                    x30=0
+                                                                                                                                    x31=0
+                                                                                                                                    x32=0
+                                                                                                                                    x33=0
+                                                                                                                                    x34=0
+                                                                                                                                    x35=0
+                                                                                                                                    x36=0
+                                                                                                                                    x37=0
+                                                                                                                                    x38=0
+                                                                                                                                    x39=0
+                                                                                                                                    x40=0
+                                                                                                                                    x41=0
+                                                                                                                                    x42=0
+                                                                                                                                    x43=0
+                                                                                                                                    x44=0
+                                                                                                                                    x45=0
+                                                                                                                                    x46=0
+                                                                                                                                    x47=0
+
+                                                                                                                                    
+
+                                                                                                                                    
+
+                                                                                                                                    x2 = my_function(x1,efef,0,1)
+                                                                                                                                    x22 = my_function(x1,efef,1,2)
+                                                                                                                                    x222 = my_function(x1,efef,2,3)
+                                                                                                                                    x2222 = my_function(x1,efef,3,4)
+                                                                                                                                    x22222 = my_function(x1,efef,4,5)
+                                                                                                                                    x21=my_function(x1,efef,5,6)
+                                                                                                                                    x22=my_function(x1,efef,6,7)
+                                                                                                                                    x23=my_function(x1,efef,7,8)
+                                                                                                                                
+                                                                                                                                    x24=my_function(x1,efef,8,9)
+                                                                                                                                    x25=my_function(x1,efef,9,12)
+                                                                                                                                    x26=my_function(x1,efef,10,11)
+                                                                                                                                    x27=my_function(x1,efef,11,12)
+                                                                                                                                    x28=my_function(x1,efef,12,13)
+                                                                                                                                    x29=my_function(x1,efef,13,14)
+                                                                                                                                    x30=my_function(x1,efef,14,15)
+                                                                                                                                    x31=my_function(x1,efef,15,16)
+                                                                                                                                    x32=my_function(x1,efef,16,17)
+                                                                                                                                    x33=my_function(x1,efef,17,18)
+                                                                                                                                    x34=my_function(x1,efef,18,19)
+                                                                                                                                    x35=my_function(x1,efef,19,20)
+                                                                                                                                    x36=my_function(x1,efef,20,21)
+                                                                                                                                    x37=my_function(x1,efef,21,22)
+                                                                                                                                    x38=my_function(x1,efef,22,23)
+                                                                                                                                    x39=my_function(x1,efef,23,24)
+                                                                                                                                    x40=my_function(x1,efef,24,25)
+                                                                                                                                    x41=my_function(x1,efef,25,26)
+                                                                                                                                    x42=my_function(x1,efef,26,27)
+                                                                                                                                    x43=my_function(x1,efef,27,28)
+                                                                                                                                    x44=my_function(x1,efef,28,29)
+                                                                                                                                    x45=my_function(x1,efef,29,30)
+                                                                                                                                    x46=my_function(x1,efef,30,31)
+                                                                                                                                    x47=my_function(x1,efef,31,32)
+
+                                                                                                                                    if x2==1 and x22==1 and x222==1 and x2222==1 and x22222==1 and x21==1 and x22==1 and x23==1 and x24==1 and x25==1 and x26==1 and x27==1 and x28==1 and x29==1 and x30==1 and x31==1 and x32==1 and x33==1 and x34==1 and x35==1 and x36==1 and x37==1 and x38==1 and x39==1 and x40==1 and x41==1 and x42==1 and x43==1 and x44==1 and x45==1 and x46==1 and x47==1:
+                                                                                                                                        total = total+1
+
+                                                                                                                                    efef=[]
+
+        
+
+print(total)
