ba06557a8285301d9c17b65cca6bd4e9a3766096
Jorgediamanto <99408709+Jorgediamanto@users.noreply.github.com>
2022-09-09 20:48:57 +0200

    Add files via upload

--- caballos2.py
+++ caballos2.py
@@ -1,7 +1,7 @@
 # 5 movimientos
 
 total=0
-verga = []
+efef = []
 
 x1 = [[1,6],[1,8],[2,7],[2,9],[3,4],[3,8],[4,3],[4,9],[4,0],[6,1],[6,7],[6,0],[7,2],[7,6],[8,1],[8,3],[9,4],[9,2],[0,4],[0,6]]
 
@@ -12,12 +12,12 @@ for x in range(10):
             for x4 in range(10):
                 for x5 in range(10):
                     for x6 in range(10):
-                        verga.append(x)
-                        verga.append(y)
-                        verga.append(x3)
-                        verga.append(x4)
-                        verga.append(x5)
-                        verga.append(x6)
+                        efef.append(x)
+                        efef.append(y)
+                        efef.append(x3)
+                        efef.append(x4)
+                        efef.append(x5)
+                        efef.append(x6)
                         #print(verga)
                         x2=0
                         x22=0
@@ -26,8 +26,8 @@ for x in range(10):
                         x22222=0
                         ddd = []
                     
-                        ddd.append(verga[0])
-                        ddd.append(verga[1])
+                        ddd.append(efef[0])
+                        ddd.append(efef[1])
 
                         for z1 in range(20):
                             if ddd==x1[z1]:
@@ -37,16 +37,16 @@ for x in range(10):
                         
                         ddd = []
                     
-                        ddd.append(verga[1])
-                        ddd.append(verga[2])
+                        ddd.append(efef[1])
+                        ddd.append(efef[2])
 
                         for z1 in range(20):
                             if ddd==x1[z1]:
                                 x22=1
 
                         ddd = []
-                        ddd.append(verga[2])
-                        ddd.append(verga[3])
+                        ddd.append(efef[2])
+                        ddd.append(efef[3])
 
                         for z1 in range(20):
                             if ddd==x1[z1]:
@@ -55,8 +55,8 @@ for x in range(10):
 
                         ddd = []
                     
-                        ddd.append(verga[3])
-                        ddd.append(verga[4])
+                        ddd.append(efef[3])
+                        ddd.append(efef[4])
 
                         for z1 in range(20):
                             if ddd==x1[z1]:
@@ -65,8 +65,8 @@ for x in range(10):
 
                         ddd = []
                     
-                        ddd.append(verga[4])
-                        ddd.append(verga[5])
+                        ddd.append(efef[4])
+                        ddd.append(efef[5])
 
                         for z1 in range(20):
                             if ddd==x1[z1]:
@@ -76,7 +76,7 @@ for x in range(10):
                         if x2==1 and x22==1 and x222==1 and x2222==1 and x22222==1:
                             total = total+1
 
-                        verga=[]
+                        efef=[]
 
         
 
